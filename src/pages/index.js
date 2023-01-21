d8f73a7c1a63ab37238c7b60c38d11c6c42f1e99
John Paul M. Batusan <johnpaul.batusa@bicol-u.edu.ph>
2023-01-22 01:10:53 +0800

    Improve and enhance main js file

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,22 @@ import { abi, NFT_CONTRACT_ADDRESS } from '../../constants'
 import styles from '../styles/Home.module.css'
 import { v4 as uuidV4 } from 'uuid'
 import { BsCheckLg, BsHandThumbsUp, BsTrashFill } from 'react-icons/bs'
+import Confetti from 'react-confetti'
+import useWindowSize from 'react-use/lib/useWindowSize'
 
 export default function Home () {
+  const [nfts, setNfts] = useState([])
   const [todos, setTodos] = useState([])
   const [todo, setTodo] = useState('')
   const [finished, setFinished] = useState(false)
   const [claimed, setClaimed] = useState(false)
   const [addToLocalStorage, setAddToLocalStorage] = useState(false)
+  const [walletConnected, setWalletConnected] = useState(false)
+  const [address, setAddress] = useState()
+  const [loading, setLoading] = useState(false)
+  const web3ModalRef = useRef()
+  const [renderConf, setRenderConf] = useState(false)
+  const { width, height } = useWindowSize()
 
   useEffect(() => {
     if (getFromStorage('Todos')) {
@@ -26,15 +35,71 @@ export default function Home () {
       if (index === 0) {
         return true
       } else {
-        setFinished(el.completed === true && arr[index - 1].completed === true)
+        setFinished(
+          el.completed === true &&
+            arr[index - 1].completed === true &&
+            !arr[index].deleted &&
+            !arr[index - 1].deleted
+        )
         return el.completed === true && arr[index - 1].completed === true
       }
     }
     if (addToLocalStorage) {
       setLocalStorage('Todos', JSON.stringify(todos))
     }
-    console.log(todos)
   }, [todos])
+
+  const shortenAdd = address => {
+    const textAddr = '' + address + ''
+    if (address) {
+      return (
+        textAddr.substring(0, 5) +
+        '......' +
+        textAddr.substring(32, address?.length)
+      )
+    }
+  }
+
+  const renderNFTs = () => {
+    console.log('fetching ...')
+    if (typeof nfts === 'object' && nfts.length !== 0) {
+      return (
+        <div style={{ backgroundColor: 'rgb(22, 23, 34)', padding: '20px 0' }}>
+          <h1
+            className={`text-center ${styles.title}`}
+            style={{ margin: '20px' }}
+          >
+            Your NFT(s)
+          </h1>
+          <div className='d-flex j-content-center'>
+            {nfts.map(nft => {
+              return (
+                <div key={nft.id}>
+                  <img
+                    src={`${process.env.IMG_URL}${nft.tokenID}.png`}
+                    width={100}
+                    height={100}
+                  />
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      )
+    }
+  }
+  const fetchNft = () => {
+    fetch(
+      `https://api-goerli.etherscan.io/api?module=account&action=tokennfttx&contractaddress=${process.env.CONTRACT_ADDR}&address=${address}&page=1&offset=100&startblock=0&endblock=99999999&sort=asc&apikey=${process.env.GOERLI_API_KEY}`
+    )
+      .then(res => res.json())
+      .then(result => {
+        setNfts(result.result)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
 
   const setLocalStorage = (key, value) => {
     if (typeof window !== 'undefined') {
@@ -102,10 +167,6 @@ export default function Home () {
     setTodos([])
   }
 
-  const [walletConnected, setWalletConnected] = useState(false)
-  const [loading, setLoading] = useState(false)
-  const web3ModalRef = useRef()
-
   const claimNFT = async () => {
     try {
       const signer = await getProviderOrSigner(true)
@@ -118,6 +179,7 @@ export default function Home () {
       await tx.wait()
       setLoading(false)
       setClaimed(true)
+      setRenderConf(true)
       removeLists()
       window.alert('You successfully claimed your NFT reward!')
     } catch (err) {
@@ -127,18 +189,12 @@ export default function Home () {
 
   const connectWallet = async () => {
     try {
-      await getProviderOrSigner()
-      setWalletConnected(true)
-    } catch (err) {
-      console.error(err)
-    }
-  }
-  const getTokenIdsMinted = async () => {
-    try {
-      const provider = await getProviderOrSigner()
-      const nftContract = new Contract(NFT_CONTRACT_ADDRESS, abi, provider)
-      const _tokenIds = await nftContract.tokenIds()
-      setTokenIdsMinted(_tokenIds.toString())
+      const providerOrSigner = await getProviderOrSigner()
+      const address = providerOrSigner.provider.selectedAddress
+      setAddress(address)
+      if (address) {
+        setWalletConnected(true)
+      }
     } catch (err) {
       console.error(err)
     }
@@ -147,7 +203,6 @@ export default function Home () {
   const getProviderOrSigner = async (needSigner = false) => {
     const provider = await web3ModalRef.current.connect()
     const web3Provider = new providers.Web3Provider(provider)
-
     const { chainId } = await web3Provider.getNetwork()
     if (chainId !== 5) {
       window.alert('Change the network to Goerli')
@@ -168,24 +223,27 @@ export default function Home () {
         providerOptions: {},
         disableInjectedProvider: false
       })
-      connectWallet()
-
-      getTokenIdsMinted()
-
-      setInterval(async function () {
-        await getTokenIdsMinted()
-      }, 5 * 1000)
     }
-  }, [walletConnected])
+    fetchNft()
+    renderButton()
+  }, [walletConnected, claimed])
 
+  const RenderConnectBtn = () => {
+    return (
+      <button onClick={connectWallet} className={`${styles.button} w-100`}>
+        Connect your wallet
+      </button>
+    )
+  }
+  const renderConfetti = () => {
+    setTimeout(() => {
+      setRenderConf(false)
+    }, 5000)
+    if (claimed && renderConf) {
+      return <Confetti width={width} height={height} />
+    }
+  }
   const renderButton = () => {
-    if (!walletConnected) {
-      return (
-        <button onClick={connectWallet} className={styles.button}>
-          Connect your wallet
-        </button>
-      )
-    }
     if (claimed) {
       return (
         <button
@@ -204,7 +262,7 @@ export default function Home () {
       return <button className={`w-100 ${styles.button}`}>Claiming...</button>
     }
 
-    if (finished && !claimed) {
+    if (finished && !claimed && walletConnected) {
       return (
         <button className={`w-100 ${styles.button}`} onClick={claimNFT}>
           Claim NFT!
@@ -216,7 +274,10 @@ export default function Home () {
   const renderButtonRm = () => {
     if (finished && todos.length !== 0) {
       return (
-        <button onClick={removeLists} className={styles.trash}>
+        <button
+          onClick={removeLists}
+          className={`${styles.trash} d-flex w-100 j-content-center`}
+        >
           <BsTrashFill />
         </button>
       )
@@ -227,12 +288,20 @@ export default function Home () {
     <>
       <Head>
         <title>My Todos</title>
-        <meta name='description' content='My Todos DApp' />
+        <meta name='og:title' content='My Todos DApp' />
+        <meta
+          name='description'
+          content='My Todos DApp - Claim an NFT reward for every full list that is completed!'
+        />
+        <meta property='og:image' content='/mytodos.gif' />
+        <meta name='twitter:image' content='/mytodos.gif' />
         <link rel='icon' href='/favicon.png' />
       </Head>
-      <div className={styles.main}>
+      <div className={styles.main} id='main'>
         <div className={styles.card}>
+          {renderConfetti()}
           <h1 className={styles.title}>My Todos</h1>
+          {shortenAdd(address)}
           <p>Be rewarded with an NFT!</p>
           <div className={styles.description}>
             <form className='d-flex'>
@@ -248,44 +317,48 @@ export default function Home () {
             {todos
               .filter(({ deleted }) => !deleted)
               .map(({ todo, completed, id }) => (
-                <div
-                  className='d-flex'
-                  style={{ maxWidth: '19.6rem' }}
-                  key={id}
-                >
-                  <p className='m-auto'>{todo}</p>
+                <div className='d-flex' key={id}>
+                  <p className='m-auto' style={{ width: '10.2rem' }}>
+                    {todo}
+                  </p>
                   {completed ? (
                     <BsHandThumbsUp
                       className='my-auto'
                       style={{ marginRight: '5px' }}
                     />
                   ) : (
+                    <div style={{ margin: 'auto 0' }}>
+                      <button
+                        onClick={() => {
+                          setCompleted(id)
+                        }}
+                        className={styles.completed}
+                      >
+                        <BsCheckLg />
+                      </button>
+                    </div>
+                  )}
+                  <div style={{ margin: 'auto 0' }}>
                     <button
                       onClick={() => {
-                        setCompleted(id)
+                        setDeleted(id)
                       }}
-                      className={styles.completed}
+                      className={styles.trash}
                     >
-                      <BsCheckLg />
+                      <BsTrashFill />
                     </button>
-                  )}
-                  <button
-                    onClick={() => {
-                      setDeleted(id)
-                    }}
-                    className={styles.trash}
-                  >
-                    <BsTrashFill />
-                  </button>
+                  </div>
                 </div>
               ))}
             <div className='d-flex'>
+              {!walletConnected ? <RenderConnectBtn /> : ''}
               {renderButton()}
               {renderButtonRm()}
             </div>
           </div>
         </div>
       </div>
+      {renderNFTs()}
     </>
   )
 }
